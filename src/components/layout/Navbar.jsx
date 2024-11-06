5194fc0c81d5f9f4b3c04099ed1079e056452247
mohamed-amine-mounir <mohamedaminemounir7@gmail.com>
2024-11-06 10:25:52 +0100

    feat: add authentication buttons to navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,8 +6,9 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between h-16 items-center">
           <Link to="/" className="text-xl font-bold">
-            Amine
+            DEV SHOP
           </Link>
+
           <div className="hidden md:flex space-x-4">
             <Link to="/" className="text-gray-600 hover:text-gray-900">
               Home
@@ -17,6 +18,18 @@ function Navbar() {
             </Link>
             <Link to="/cart" className="text-gray-600 hover:text-gray-900">
               Cart
+            </Link>
+          </div>
+
+          <div className="hidden md:flex space-x-4">
+            <Link to="/login" className="text-gray-600 hover:text-gray-900">
+              Login
+            </Link>
+            <Link
+              to="/register"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+            >
+              Register
             </Link>
           </div>
         </div>
