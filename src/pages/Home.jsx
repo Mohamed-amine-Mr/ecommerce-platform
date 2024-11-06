f3125251fa73b456094b2942725b9649cf253f06
mohamed-amine-mounir <mohamedaminemounir7@gmail.com>
2024-11-06 12:04:37 +0100

    feat: restructure Home component by adding Hero, Categories, FeaturedProducts, DealOfTheDay, LatestProducts, and Newsletter

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Hero from "../components/layout/Hero";
+import Categories from "../components/product/Categories";
+import FeaturedProducts from "../components/product/FeaturedProducts";
+import DealOfTheDay from "../components/product/DealOfTheDay";
+import LatestProducts from "../components/product/LatestProducts";
+import Newsletter from "../components/common/Newsletter";
+
+const Home = () => (
+  <div className="min-h-screen bg-gray-50">
+    <Hero />
+    <Categories />
+    <FeaturedProducts />
+    <DealOfTheDay />
+    <LatestProducts />
+    <Newsletter />
+  </div>
+);
+
+export default Home;
