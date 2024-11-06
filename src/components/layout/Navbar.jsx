f1ab60b98708707ac6042bae0f586f50430bcbac
mohamed-amine-mounir <mohamedaminemounir7@gmail.com>
2024-11-06 10:17:04 +0100

    feat: add basic navbar structure in layout folder

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav className="bg-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between h-16 items-center">
+          <div className="text-xl font-bold">Your Shop</div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
