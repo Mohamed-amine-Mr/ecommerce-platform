6dffd149900c02875bf892ff440eeffbfa395ee9
mohamed-amine-mounir <mohamedaminemounir7@gmail.com>
2024-11-06 10:18:38 +0100

    feat: integrate navbar into main app layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar"; // Adjust path as necessary
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes></Routes>
     </Router>
   );
